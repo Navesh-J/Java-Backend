a183a507fde2a9caff507a3535aa814b6899fcbf
Navesh Jaiswal <navesh.professional@gmail.com>
2026-01-06 02:04:39 +0530

    SpringBoot JDBC

---
+++ SpringBoot/JDBC/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollno,name,marks) values (101,'Navesh',85);
+insert into student (rollno,name,marks) values (102,'Anshul',56);
+insert into student (rollno,name,marks) values (103,'Prince',90);
