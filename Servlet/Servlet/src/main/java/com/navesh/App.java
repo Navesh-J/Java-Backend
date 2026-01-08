24c7755f5cb1f90215cd8a493cfb7c0607b3597d
Navesh Jaiswal <navesh.professional@gmail.com>
2026-01-08 06:30:29 +0530

    Web Servlets

--- Servlet/Servlet/src/main/java/com/navesh/App.java
+++ Servlet/Servlet/src/main/java/com/navesh/App.java
@@ -1,13 +1,22 @@
 package com.navesh;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws LifecycleException {
+        System.out.println("Hello World");
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("",null);
+        Tomcat.addServlet(context,"HelloServlet",new HelloServlet());
+        context.addServletMappingDecoded("/hello","HelloServlet");
+
+        tomcat.getConnector();
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
--- Servlet/Servlet/src/main/java/com/navesh/HelloServlet.java
+++ Servlet/Servlet/src/main/java/com/navesh/HelloServlet.java
@@ -1,4 +1,19 @@
 package com.navesh;
 
-public class HelloServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("In Service");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h1><i>Hello World</i></h1>");
+    }
 }
