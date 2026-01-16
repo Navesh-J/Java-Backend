cf2928cf62e9732e69a368f089b88cc3b431e2ea
Navesh Jaiswal <navesh.professional@gmail.com>
2026-01-16 22:13:38 +0530

    Spring JPA (Java Persistence API)

---
+++ SpringBoot/JPA/src/main/java/com/navesh/spring_data_jpa/StudentRepo.java
@@ -0,0 +1,14 @@
+package com.navesh.spring_data_jpa;
+
+import com.navesh.spring_data_jpa.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student,Integer> {
+
+    List<Student> findByName(String name);
+    List<Student> findByMarks(int marks);
+}
